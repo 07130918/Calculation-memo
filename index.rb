8a7c3e21e266e016201d0e9c3cc2adf0f2b0c6b6
EC2 Default User <ec2-user@ip-172-31-1-72.ap-northeast-1.compute.internal>
2020-10-16 03:06:09 +0000

    This and that of the Fibonacci sequence

--- index.rb
+++ index.rb
@@ -8,6 +8,8 @@ end
 
 number(1000)
 # https://docs.ruby-lang.org/ja/latest/library/prime.html
+puts "-------------------------------------------"
+
 # ----------------------------------------
 # 任意の数(maxNumber)までで何回その数字(total)が出力されたか？
 def countNumber(maxNumber, total)
@@ -17,6 +19,8 @@ def countNumber(maxNumber, total)
 end
 # 例)11までで何回1が出力されたか
 countNumber(11, 1)
+puts "-------------------------------------------"
+
 # -------------------------------------------
 # 世界のナベアツになり1から40まで数える
 def world
@@ -31,6 +35,8 @@ def world
 end
 
 world
+puts "-------------------------------------------"
+
 # ----------------------------------------------
 # 任意の約数の合計値
 def divisor(number)
@@ -41,16 +47,20 @@ def divisor(number)
     i += 1
   end
   
- puts result.sum
+puts result.sum
 end
 
 divisor(4)
+puts "-------------------------------------------"
+
 # -------------------------------------------------
 # 逆数和 -> 1/1...1/nの合計で9を超える
 def array(n)
     (1..n).map{|n| Rational(1,n)}.sum.to_f
 end
 puts array(4550)
+puts "-------------------------------------------"
+
 # --------------------------------------------
 # 任意の数(今回は30)までFizzBuzz(アメリカの暇つぶし言葉遊び)
 def fizzBuzz(num)
@@ -69,7 +79,35 @@ end
 
 fizzBuzz(30)
 # https://ja.wikipedia.org/wiki/Fizz_Buzz
+puts "-------------------------------------------"
 
 # --------------------------------------------
-# フィボナッチ数列(任意の項までの和)
+# フィボナッチ数列のあれこれ
+# 任意の項までのフィボナッチ数列とその和を出力,任意の数値がフィボナッチ数列に含まれるか判定
+fib = Enumerator.new do |y|
+  a = 0; b = 1
+  loop do
+    y << a
+    a, b = b, a + b
+  end
+end
 
+puts "数値を入力してください その項数までのフィボナッチ数列,その合計を出力します"
+num = gets.chomp.to_i  
+fibo = fib.take(num)
+p fibo
+puts "total: #{fibo.sum}"
+
+puts "数値を入力してください 数値がフィボナッチ数列にあるかないか調べます(100項まで)。ある場合は何項目か調べます。"
+what = gets.chomp.to_i
+ex = fib.take(100)
+if ex.include?(what)
+  puts "あります 2,3項目です" if what ==  1
+  existence =  ex.index(what) + 1 # 入力された数値のインデックス番号を取得1を足してはじまりは1番とする
+  puts "あります #{existence}項目です" unless what == 1 
+else
+  puts "ありません"
+end  
+puts "-------------------------------------------"
+
+# ----------------------------------------------------
