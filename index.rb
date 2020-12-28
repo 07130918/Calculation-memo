f85f5ed1010425af3369e864aba1bd34ccb2e4a6
kotawindows <tokyosato1@gmail.com>
2020-12-28 09:26:43 +0900

    :zap:

--- index.rb
+++ index.rb
@@ -15,7 +15,7 @@ puts "-------------------------------------------"
 def countNumber(maxNumber, total)
     totalNumber = total.to_s
     num = [*(1..maxNumber)].to_s
-    puts num.count(totalNumber) 
+    puts num.count(totalNumber)
 end
 # 例)11までで何回1が出力されたか
 countNumber(11, 1)
@@ -42,11 +42,11 @@ puts "-------------------------------------------"
 def divisor(number)
   result = []
   i = 1
-  while i <= number 
+  while i <= number
     result << i if number % i == 0
     i += 1
   end
-  
+
 puts result.sum
 end
 
@@ -54,12 +54,20 @@ divisor(4)
 puts "-------------------------------------------"
 
 # -------------------------------------------------
-# 逆数和 -> 1/1...1/nの合計で9を超える
-def array(n)
-    (1..n).map{|n| Rational(1,n)}.sum.to_f
+逆数和 -> 1/1...1/nの合計で9を超える
+def method
+  total = 0
+  n = 1
+  while 9 > total
+    total+= (1 / n.to_f)
+    n += 1
+  end
+  puts n
+  puts total
 end
-puts array(4550)
-puts "-------------------------------------------"
+method
+
+puts '-------------------------------------------'
 
 # --------------------------------------------
 # 任意の数(今回は30)までFizzBuzz(アメリカの暇つぶし言葉遊び)
@@ -73,7 +81,7 @@ def fizzBuzz(num)
       puts "Buzz"
     else
       puts n
-    end  
+    end
   end
 end
 
@@ -93,7 +101,7 @@ fib = Enumerator.new do |y|
 end
 
 puts "数値を入力してください その項数までのフィボナッチ数列,その合計を出力します"
-num = gets.chomp.to_i  
+num = gets.chomp.to_i
 fibo = fib.take(num)
 p fibo
 puts "total: #{fibo.sum}"
@@ -104,11 +112,11 @@ ex = fib.take(100)
 if ex.include?(what)
   puts "あります 2,3項目です" if what ==  1
   existence =  ex.index(what) + 1 # 入力された数値のインデックス番号を取得1を足してはじまりは1番とする
-  puts "あります #{existence}項目です" unless what == 1 
+  puts "あります #{existence}項目です" unless what == 1
 else
   puts "ありません"
-end  
+end
 puts "-------------------------------------------"
 
 # 4乗的ガウス
-puts (1..80).inject(0) {|result, item| result + item**4 } 
+puts (1..80).inject(0) {|result, item| result + item**4 }
